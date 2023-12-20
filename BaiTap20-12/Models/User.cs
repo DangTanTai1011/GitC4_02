1c36550549d18521feba41834d64e328dd552edb
Your Name <you@example.com>
2023-12-20 14:28:21 +0700

    Create File User.cs in Models

---
+++ BaiTap20-12/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTap20_12.Models
+{
+    internal class User
+    {
+		private int id;
+
+		public int ID
+		{
+			get { return id; }
+			set { id = value; }
+		}
+
+	}
+}
